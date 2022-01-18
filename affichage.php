413463308d9d778b0c812eb063996cf2589b841b
kante24 <kantedavid@gmail.com>
2022-01-18 13:41:21 -0500

    -

--- affichage.php
+++ affichage.php
@@ -1,6 +1,7 @@
+<!-- Créer une table dans la bd avec nom, type, taille en int et bin en longblob. -->
+
 <!DOCTYPE html>
 <html lang="fr">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
